f7551eb2a694fd409e83d19c2a49d2213b0bacc8
Jonathan Leon <jonathanvarga42@hotmail.com>
2026-09-25 10:16:43 -0500

    Added docker setup and airlfow to the project

--- video_stats.py
+++ video_stats.py
@@ -7,7 +7,7 @@ from datetime import date
 load_dotenv(dotenv_path= "./.env")
 
 API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+CHANNEL_HANDLE =  os.getenv("CHANNEL_HANDLE")
 maxResults = 50
 
 def get_playlist_id():
